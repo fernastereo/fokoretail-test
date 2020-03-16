48c2450d47b5b8c13feaf9f4b85c3c262c2d7454
fernastereo <fernandoecueto@gmail.com>
2020-03-16 00:46:01 +0000

    Created vue components for contacts, contactslist and activeconversation

---
+++ resources/js/components/ActiveConversationComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-row>
+    <b-col cols="8"></b-col>
+    <b-col cols="4">
+      <b-img rounded="circle" blank width="50" height="50" blank-color="#777" alt="img" class="m-1"></b-img>
+      <p>Usuario Seleccionado</p>
+      <hr>
+      <b-form-checkbox
+          id="checkbox-1"
+          v-model="status"
+          name="checkbox-1"
+          value="accepted"
+          unchecked-value="not_accepted"
+          >
+          I accept the terms and use
+      </b-form-checkbox>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+    export default {
+      data(){
+        return {
+        }
+      },
+      mounted() {
+          console.log('Component mounted.')
+      }
+    }
+</script>
---
+++ resources/js/components/ContactComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <b-list-group-item class="p-0" :variant="variant">
+        <b-row class="p-0">
+            <b-col cols="12" md="3" class="text-center">
+                <b-img rounded="circle" blank width="50" height="50" blank-color="#777" alt="img" class="m-1"></b-img>
+            </b-col>
+            <b-col cols="6" align-self="center" class="d-none d-md-block">
+                <p class="mb-0">{{ name }}</p>
+                <p class="text-muted small mb-0">{{ lastMessage }}</p>
+            </b-col>
+            <b-col cols="3" class="d-none d-md-block">
+                <p class="text-muted small mt-1">{{ lastTime }}</p>
+            </b-col>
+        </b-row>        
+    </b-list-group-item>
+</template>
+
+<script>
+    export default {
+      props: [
+        'variant'
+      ],
+      data(){
+        return {
+          name: 'Carlos Bacca',
+          lastMessage: 'Proximo Partido en abril',
+          lastTime: '10:56 a.m.',
+        }
+      },
+      mounted() {
+          console.log('Component mounted.')
+      }
+    }
+</script>
