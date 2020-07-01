6d5e7820f635ec7f0266bc7ddf417d05ce5aca1e
fernastereo <fernandoecueto@gmail.com>
2020-06-30 21:32:24 -0500

    se cambio configuracion del filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,7 +45,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/public/users'),
+            'root' => storage_path('app'),
         ],
 
         'public' => [
